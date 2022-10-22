4be0b6dcf5786e6095536a42bf804cb27c3131a9
Juan Peralta <juanperalta2015f@gmail.com>
2022-10-22 03:51:05 -0300

    Modelado de entidades, css

---
+++ Data/Usuarios.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace paginamodelo.Data
+{
+    public class Usuarios
+    {
+        public int Id { get; set; }
+        public int Nombre { get; set; }
+
+    }
+}
